2feef01f1c4ecce5dcbeb596596fcfbada838ed7
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-06-01 03:11:46 +0500

    Update main.py

--- class_03/main.py
+++ class_03/main.py
@@ -1,6 +1,42 @@
-def main():
-    print("Hello from class-03!")
+from agents import Runner, Agent, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
+from dotenv import load_dotenv
+import os
+import chainlit as cl
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+agent = Agent(
+    name="Subhan Kaladi",
+    instructions="Greeting from subhan kaladi",
+    model=model
+    
+)
+
+
+@cl.on_message
+async def handle_message(message: cl.Message):
+    result = await Runner.run(
+        agent,
+        input=message.content,
+        run_config=config
+    )
+    await cl.Message(content=result.final_output).send()
