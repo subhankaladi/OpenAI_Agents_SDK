f5c39a599851f2c7cd6fa5888b8337c68a6913de
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-06-21 21:10:06 +0500

    Add ToolCall

--- 00_TopicWise_Code/03_ToolCall/main.py
+++ 00_TopicWise_Code/03_ToolCall/main.py
@@ -1,6 +1,65 @@
-def main():
-    print("Hello from 03-toolcall!")
+from agents import Agent, Runner, OpenAIChatCompletionsModel, AsyncOpenAI, set_tracing_disabled, function_tool
+import os
+from dotenv import load_dotenv
+import requests
+import random
 
 
-if __name__ == "__main__":
-    main()
+
+load_dotenv()
+set_tracing_disabled(disabled=True)
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=provider
+)
+
+
+@function_tool
+def how_many_jokes():
+    """
+    Get Random Number for jokes
+    """
+    return random.randint(1, 10)
+
+@function_tool
+def get_weather(city: str) -> str:
+    """
+    Get the weather for a given city
+    """
+    try:
+        result = requests.get(
+            f"http://api.weatherapi.com/v1/current.json?key=8e3aca2b91dc4342a1162608252604&q={city}"
+        )
+
+        data = result.json()
+
+        return f"The current weather in {city} is {data["current"]["temp_c"]}C with {data["current"]["condition"]["text"]}."
+    
+    except Exception as e :
+        return f"Could not fetch weather data due to {e}"
+agent = Agent(
+    name="Assistant",
+    instructions="""
+if the user asks for jokes, first call 'how_many_jokes' function, then tell that jokess with numbers.
+if the user asks for weather, call the 'get_weather' funciton with city name
+""",
+    model=model,
+    tools=[get_weather, how_many_jokes]
+
+)
+
+result = Runner.run_sync(
+    agent,
+    input="tell me karachi weather",
+)
+
+print(result.final_output)
