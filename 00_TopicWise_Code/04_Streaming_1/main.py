0da4f4bacf6d254d28f4904fdbdce0f78552e742
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-06-22 02:56:33 +0500

    Add Streaming-1

--- 00_TopicWise_Code/04_Streaming_1/main.py
+++ 00_TopicWise_Code/04_Streaming_1/main.py
@@ -1,6 +1,58 @@
-def main():
-    print("Hello from 04-streaming-1!")
+from agents import Runner, Agent, AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig
+from dotenv import load_dotenv
+import os
+import chainlit as cl
+from openai.types.responses import ResponseTextDeltaEvent
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+agent = Agent(
+    name="Subhan Kaladi",
+    instructions="Greeting from subhan kaladi",
+    model=model
+    
+)
+
+@cl.on_chat_start
+async def handle_start_chat():
+    cl.user_session.set("history" ,[])
+    await cl.Message(content="Hello from subhan kaladi").send()
+
+@cl.on_message
+async def handle_message(message: cl.Message):
+    history = cl.user_session.get("history")
+
+    msg = cl.Message(content="")
+    await msg.send()
+
+    history.append({"role": "user", "content":message.content})
+    result = Runner.run_streamed(
+        agent,
+        input=history,
+        run_config=config
+    )
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            await msg.stream_token(event.data.delta)
+    history.append({"role": "user", "content": result.final_output})
+    cl.user_session.set("history", history)
+    # await cl.Message(content=result.final_output).send()
