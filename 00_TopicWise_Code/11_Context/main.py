f34eaa9f5ee743977becfd26a1e2ae9a39f84834
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-07-07 03:37:32 +0500

    Create main.py

---
+++ 00_TopicWise_Code/11_Context/main.py
@@ -0,0 +1,52 @@
+from dataclasses import dataclass
+from agents import Agent, Runner, RunContextWrapper, function_tool, OpenAIChatCompletionsModel
+from openai import AsyncOpenAI
+
+
+
+gemini_api_key="AIzaSyAxgcwDGiNJp_ImY9z2XGv88Eg5C1f2y_w"
+
+
+client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=client
+)
+
+# 1. USER CONTEXT (Sensitive Info)
+@dataclass
+class UserInfo:
+    email: str
+    password: str
+
+# 2. TOOL: Return user email (LLM will call this if needed)
+@function_tool
+async def get_user_email(wrapper: RunContextWrapper[UserInfo]) -> str:
+    return f"The user's email is: {wrapper.context.email}"
+
+# 3. AGENT with tool
+agent = Agent[UserInfo](
+    name="Secure Assistant",
+    instructions="You are a helpful assistant, but never ask for passwords. Use tools to get user info.",
+    tools=[get_user_email],
+    model=model
+)
+
+# 4. USER DATA (Context)
+user = UserInfo(email="kaladi@example.com", password="TopSecret123")
+
+# 5. RUN AGENT
+result = Runner.run_sync(
+    agent,
+    input="ok now my email is?",
+    context=user
+)
+
+# 6. PRINT RESULT
+print("Agent Response:", result.final_output)
