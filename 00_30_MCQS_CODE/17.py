a8fe80a099aa4aafb575b1f39c104738fe9bbdc3
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-09-13 20:08:27 +0500

    Create 17.py

---
+++ 00_30_MCQS_CODE/17.py
@@ -0,0 +1,40 @@
+import asyncio
+from agents import Agent, ModelSettings, Runner, enable_verbose_stdout_logging, function_tool
+from dotenv import load_dotenv
+import os
+from agents import OpenAIChatCompletionsModel, set_tracing_disabled
+from dotenv import load_dotenv
+from openai import AsyncOpenAI
+
+load_dotenv()
+set_tracing_disabled(disabled=True)
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+# enable_verbose_stdout_logging()
+
+
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=provider
+)
+
+
+
+
+
+
+agent = Agent(
+    name="Assistant",
+    model=model
+)
+
+result = Runner.run_sync(
+    agent,
+    "what is current weather in karachi "
+)
+
+print(result.last_agent)
