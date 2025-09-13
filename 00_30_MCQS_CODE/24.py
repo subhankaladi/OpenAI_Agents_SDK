b10a6e3e27c67de7283c217fda527cbd97d9c232
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-09-13 20:10:51 +0500

    update

--- 00_30_MCQS_CODE/24.py
+++ 00_30_MCQS_CODE/24.py
@@ -8,7 +8,7 @@ load_dotenv()
 agent = Agent(
     name="Assistant",
     instructions="you are helpful assistant.",
-    tools=[FileSearchTool(vector_store_ids=["vs_68b0902f0b0481919a3a4759fe3adcff"], max_num_results=3)]
+    tools=[FileSearchTool(vector_store_ids=["your_ids"], max_num_results=3)]
 )
 
 result = Runner.run_sync(
