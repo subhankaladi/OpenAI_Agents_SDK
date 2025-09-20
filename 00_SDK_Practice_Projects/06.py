31fb1ff7fcbc94709cef5fab28a696e516859c2b
Subhan Kaladi <153628962+subhankaladi@users.noreply.github.com>
2025-09-21 00:55:53 +0500

    Create 06.py

---
+++ 00_SDK_Practice_Projects/06.py
@@ -0,0 +1,30 @@
+from agents import Agent, Runner, function_tool, ModelSettings, enable_verbose_stdout_logging
+from dotenv import load_dotenv
+
+load_dotenv()
+enable_verbose_stdout_logging()
+
+
+@function_tool
+def get_weather(city:str):
+    """get weather for given number"""
+
+    return f"the current weather in {city} is cloudy."
+
+
+main_agent = Agent(
+    name="Assistant",
+    instructions="You are helpful assistant.",
+    tools=[get_weather],
+    model_settings=ModelSettings(
+        tool_choice="required"
+    )
+)
+
+result = Runner.run_sync(
+    main_agent,
+    "hello?",
+    max_turns=1
+)
+
+print(result.final_output)
