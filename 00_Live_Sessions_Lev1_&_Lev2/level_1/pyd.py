143b062054ab4d1390a42d610d1506c68e2957ff
subhankaladi <subhankaladi@gmail.com>
2025-12-26 21:44:35 +0500

    Create pyd.py

---
+++ 00_Live_Sessions_Lev1_&_Lev2/level_1/pyd.py
@@ -0,0 +1,35 @@
+
+
+# class User:
+#     def __init__(self, name: str, id : int):
+#         self.name = name
+#         self.id = id
+
+# u = User(name=10, id="subhan")
+# print(u.name)
+
+
+
+# from pydantic import BaseModel
+
+
+# class User(BaseModel):
+#     name : str
+#     id : int
+
+# u = User(id=10, name="subhan")
+# print(u.name)
+
+
+
+
+
+# from dataclasses import dataclass
+
+# @dataclass
+# class User:
+#     name : str
+#     id : int
+
+# u = User(name=10, id="subhan")
+# print(u.name)
