08544c4de5408a76faf95651600e4e3574630512
subhankaladi <subhankaladi@gmail.com>
2025-12-26 21:44:40 +0500

    Create usecase.py

---
+++ 00_Live_Sessions_Lev1_&_Lev2/level_1/usecase.py
@@ -0,0 +1,13 @@
+# from pydantic.dataclasses import dataclass
+# # from dataclasses import dataclass
+
+# @dataclass
+# class User():
+#     name : str
+#     id : int
+
+# user = User(name="subhan", id="5")
+
+# print(user.id, type(user.id))
+
+
