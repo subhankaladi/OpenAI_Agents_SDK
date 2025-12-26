212b3e745f56ead7c07e06f9e444340206521b8b
subhankaladi <subhankaladi@gmail.com>
2025-12-26 21:44:17 +0500

    Create ex1.py

---
+++ 00_Live_Sessions_Lev1_&_Lev2/level_1/ex1.py
@@ -0,0 +1,29 @@
+from agents import Agent, Runner, MaxTurnsExceeded, function_tool
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@function_tool
+def get_weather(city:str):
+    """get weather for given city"""
+
+    return f"the current weather in {city} is cloudy"
+
+
+main_agent = Agent(
+    name="Assistant",
+    instructions="you are helpfull assistant",
+    tools=[get_weather]
+)
+
+try :
+    result = Runner.run_sync(
+    main_agent,
+    "what is current weather in karachi ?",
+    max_turns=1
+)
+    print(result.final_output)
+
+except MaxTurnsExceeded as ae:
+    print(f"max turns exception is raise {ae}")
+
