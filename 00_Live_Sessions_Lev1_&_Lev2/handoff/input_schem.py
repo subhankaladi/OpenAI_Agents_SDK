32b070b2ace116562e6f3ff3ad41ceec762b828a
subhankaladi <subhankaladi@gmail.com>
2025-12-26 21:44:03 +0500

    Create input_schem.py

---
+++ 00_Live_Sessions_Lev1_&_Lev2/handoff/input_schem.py
@@ -0,0 +1,11 @@
+input_schema = {
+    "type": "object",
+    "properties": {
+        "code": {
+            "type": "string",
+            "description": "Python code snippet to execute"
+        }
+    },
+    "required": ["code"],
+    "additionalProperties": False
+}
