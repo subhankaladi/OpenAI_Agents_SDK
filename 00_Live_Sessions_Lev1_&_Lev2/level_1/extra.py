b34f3487941198eee3062cd94171cd7c07fe194a
subhankaladi <subhankaladi@gmail.com>
2025-12-26 21:44:23 +0500

    Create extra.py

---
+++ 00_Live_Sessions_Lev1_&_Lev2/level_1/extra.py
@@ -0,0 +1,5 @@
+# Extra headers for HTTP request
+# Example: custom authentication header ya tracking info.
+# Ye parameters mostly advanced use cases, debugging, ya future API features ke liye hain. Normal usage mein inki zarurat nahi hoti.
+
+extra_body={"metadata": {"user_id": "123"}},
